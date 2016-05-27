34a23896d23833f0476cdd2e5e7e3cff9a1dc649
Anton Thorsell <anton.thorsell@hotmail.com>
2016-05-27 16:41:38 +0200

    dial now foolows mouse

--- Assets/Scripts/Dial.cs
+++ Assets/Scripts/Dial.cs
@@ -5,9 +5,8 @@ using System.Collections;
 //This script will handle the movement of the dial
 
 public class Dial : MonoBehaviour {
-
-    public float TotalDegrees = 360;
-    public float AtDegree = 0;
+    
+    public int AtDegree = 0;
     public bool Active = true;
 
 	void Start ()
@@ -59,13 +58,11 @@ public class Dial : MonoBehaviour {
             float distance = heading.magnitude;
             if(distance > 0.2)
             {
-
                 //normalized
                 Vector2 direction = heading / distance;
 
-
-
-
+                float degrees = Mathf.Atan2(direction.y, direction.x);
+                transform.eulerAngles = new Vector3(0, 0, degrees);
             }
 
 
