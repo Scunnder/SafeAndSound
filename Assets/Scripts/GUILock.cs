533b9a6879ef01e5eff0edc28114a1f394e41007
Anton Thorsell <anton.thorsell@hotmail.com>
2016-05-28 22:54:53 +0200

    prefabs fuck

---
+++ Assets/Scripts/GUILock.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUILock : MonoBehaviour {
+
+    public int Lock = 0;
+    public GameObject Locked;
+    public GameObject Unlocked;
+
+    public void Start()
+    {
+        SetLocked(true);
+    }
+	
+	public void SetLocked(bool set)
+    {
+        Locked.SetActive(set);
+        Unlocked.SetActive(!set);
+    }
+}
