463f09fd3b40df82f0af237227c75f99a7806b72
Anton Thorsell <anton.thorsell@hotmail.com>
2016-05-28 20:21:57 +0200

    added a script and metadata

--- Assets/Scripts/Dial.cs
+++ Assets/Scripts/Dial.cs
@@ -4,76 +4,148 @@ using System.Collections;
 
 //This script will handle the movement of the dial
 
-public class Dial : MonoBehaviour {
-
-    public float TotalDegrees = 360;
+public class Dial : MonoBehaviour
+{
+    public float PressPoint = 0f;
+    public float StartDegree = 0f;
     public float AtDegree = 0;
     public bool Active = true;
 
-	void Start ()
+    void Start()
     {
-	
-	}
-    
+
+    }
+
     void Update()
     {
-        if(Input.touchCount > 0)
+        if (Active)
         {
-            IsTouchedFinger();
-        }
-        else if(Input.GetMouseButton(0))
-        {
-            IsTouchedMouse();
-        }
-    }
-
-
-    public void IsTouchedFinger()
-    {
-        Vector2 mp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-        Collider2D c = Physics2D.OverlapPoint(mp);
-
-        if (c != null && c.gameObject.tag == "Dial")
-        {
-            Vector2 dir = mp - (Vector2)transform.position;
-
-            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-
-            if (angle < transform.eulerAngles.z)
+            if (Input.touchCount > 0)
             {
-                transform.eulerAngles = new Vector3(0, 0, angle);
+                IsTouchedFinger();
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                MouseUpdate();
+            }
+            else
+            {
+                EndPress();
             }
         }
+        UpdateRotation();
     }
-    
-    public void IsTouchedMouse()
+
+    public void UpdateRotation()
     {
 
-        Vector2 mousepoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.eulerAngles = new Vector3(0, 0, AtDegree);
+    }
+
+
+    private bool DoOneFinger = true;
+    public float StartPoint = 0;
+    public void IsTouchedFinger()
+    {
+
+        Vector2 fingerpoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, 1000f));
+        Collider2D collider = Physics2D.OverlapPoint(fingerpoint);
+
+        if (collider != null && collider.gameObject.tag == "Dial")
+        {
+
+            Vector2 heading = fingerpoint - (Vector2)transform.position;
+            float distance = heading.magnitude;
+            if (distance > 0.5)
+            {
+                Vector2 direction = heading / distance;
+
+                float degrees = -CustomAngle(direction); //Mathf.Atan2(direction.y, direction.x) * 180;// / Mathf.PI;
+
+                degrees = FixDegrees(degrees);
+
+                if (DoOneFinger)
+                {
+                    StartPoint = degrees;
+                    DoOneFinger = false;
+                }
+                UpdatePress(degrees);
+            }
+        }
+        else
+        {
+            EndPress();
+        }
+    }
+
+
+
+    private bool DoOneMouse = true;
+    public void MouseUpdate()
+    {
+        Vector2 mousepoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1000f));
         Collider2D collider = Physics2D.OverlapPoint(mousepoint);
 
         if (collider != null && collider.gameObject.tag == "Dial")
         {
-            //get distance and heading
+
             Vector2 heading = mousepoint - (Vector2)transform.position;
             float distance = heading.magnitude;
-            if(distance > 0.2)
+            if (distance > 0.5)
             {
-
-                //normalized
                 Vector2 direction = heading / distance;
 
+                float degrees = -CustomAngle(direction); //Mathf.Atan2(direction.y, direction.x) * 180;// / Mathf.PI;
+                
 
-
-
+                if (DoOneMouse)
+                {
+                    StartDegree = AtDegree;
+                    PressPoint = degrees;
+                    DoOneMouse = false;
+                }
+                UpdatePress(degrees);
             }
+        }
+        else
+        {
+            EndPress();
+        }
+    }
+    
+    public void UpdatePress(float degrees)
+    {
+        AtDegree = FixDegrees((degrees - PressPoint) + StartDegree);
+    }
 
+    public void EndPress()
+    {
+        DoOneFinger = true;
+        DoOneMouse = true;
+    }
 
+    public float FixDegrees(float degrees)
+    {
+        while(degrees > 360)
+        {
+            degrees -= 360;
+        }
+        while (degrees < -360)
+        {
+            degrees += 360;
+        }
+        return degrees;
+    }
 
-
-
-            //float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg; //I dont get this
-            //transform.eulerAngles = new Vector3(0, 0, angle);
+    public static float CustomAngle(Vector2 p_vector2)
+    {
+        if (p_vector2.x < 0)
+        {
+            return 360 - (Mathf.Atan2(p_vector2.x, p_vector2.y) * Mathf.Rad2Deg * -1);
+        }
+        else
+        {
+            return Mathf.Atan2(p_vector2.x, p_vector2.y) * Mathf.Rad2Deg;
         }
     }
 
---
+++ Assets/Scripts/SafeLock.cs
@@ -0,0 +1,160 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SafeLock : MonoBehaviour
+{
+
+    public int MillisecondsToUnlock = 100;
+    private int Sweetspot = 0;
+    public int Highest = 99;
+    public Dial SafeDial;
+    private int TickSize = 360;
+    private int ActiveLock = 0;
+    public bool Active = true;
+    public AudioSource[] sources;
+
+    public void Start()
+    {
+        if (TickSize == 0)
+        {
+            TickSize = 360;
+        }
+        NewSweetspot(true);
+        TickSize = 8;
+    }
+
+    public int WasAt = 0;
+
+
+    public float count = 0;
+    public void Update()
+    {
+        if (Active)
+        {
+            int at = Mathf.RoundToInt(SafeDial.AtDegree / TickSize);
+            CheckDistance(at);
+
+            if (WasAt != at)
+            {
+                WasAt = at;
+                if (at == Sweetspot)
+                {
+                    Handheld.Vibrate();
+                    sources[0].Play();
+                }
+                else
+                {
+                    sources[1].pitch = Intensity;
+                    sources[1].Play();
+                }
+            }
+            WasAt = at;
+
+            //vibration
+
+
+            //check sweetspot
+            if (at == Sweetspot)
+            {
+                count += 1f * Time.deltaTime;
+            }
+            else
+            {
+                count = 0;
+            }
+
+            if (count > MillisecondsToUnlock)
+            {
+                LockDone();
+            }
+        }
+    }
+
+    public void LockDone()
+    {
+        NewSweetspot();
+    }
+
+    public void NewSweetspot(bool first = false)
+    {
+        int newsweet = Random.Range(0, Highest);
+        while (newsweet == Sweetspot)
+        {
+            newsweet = Random.Range(0, Highest);
+        }
+        Sweetspot = newsweet;
+
+        if (!first)
+        {
+            ActiveLock++;
+        }
+
+        if (ActiveLock > 2)
+        {
+            SafeUnlocked();
+        }
+        Debug.Log("Sweetspot: " + Sweetspot);
+    }
+
+
+    public float Intensity = 1;
+    public void CheckDistance(int at)
+    {
+
+        int a = Sweetspot - at;
+        if (a > 45)
+        {
+            a -= 45;
+        }
+
+        if (a < 0)
+        {
+            a += 45;
+        }
+
+        int b = at - Sweetspot;
+        if (b > 45)
+        {
+            b -= 45;
+        }
+
+        if (b < 0)
+        {
+            b += 45;
+        }
+
+        int useThis = 0;
+        if (a > b)
+        {
+            useThis = b;
+        }
+        else
+        {
+            useThis = a;
+        }
+        //result
+
+        if (useThis <= 3)
+        {
+            Intensity = 1.2f;
+        }
+        if (useThis <= 2)
+        {
+            Intensity = 1.4f;
+        }
+        if (useThis <= 1)
+        {
+            Intensity = 1.6f;
+        }
+        if (useThis == 0)
+        {
+            Intensity = 1.8f;
+        }
+    }
+
+    public void SafeUnlocked()
+    {
+        Active = false;
+    }
+}
